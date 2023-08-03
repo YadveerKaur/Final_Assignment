d6d7090ed67cce23c5e4a2c369afcd9b89d2b234
YadveerKaur <yadveerkaur5@gmail.com>
2023-08-03 16:26:56 -0400

    commit

--- server.js
+++ server.js
@@ -109,7 +109,47 @@ app.get('/about', (req, res) => {
   console.log('test about');
     res.render('about');
   });
+app.get('/login',(req,res)=>{
+  res.render('login');
+})
 
+app.get('register',(req,res)=>{
+  res.render('register');
+})
+
+app.post('/register',(req,res)=>{
+  authData.RegisterUser(req.body).then(()=>{
+    res.render('register',{successMessage:'User Creater'});
+  })
+  .catch((err) => {
+    res.render('register', { errorMessage: err, userName: req.body.userName });
+  });
+})
+
+app.post('/login', (req, res) => {
+  req.body.userAgent = req.get('User-Agent');
+  authData.CheckUser(req.body)
+    .then((user) => {
+      req.session.user = {
+        userName: user.userName,
+        email: user.email,
+        loginHistory: user.loginHistory
+      };
+      res.redirect('/items');
+    })
+    .catch((err) => {
+      res.render('login', { errorMessage: err, userName: req.body.userName });
+    });
+});
+
+app.get("/logout", function(req, res) {
+  req.session.reset();
+  res.redirect("/login");
+});
+
+app.get('/userHistory',ensureLogin,(req,res)=>{
+  res.render('userHistory');
+})
 // app.get('/shop', (req, res) => {
 //   store_service.getPublishedItems().then((data)=>{
 //     res.json(data)
